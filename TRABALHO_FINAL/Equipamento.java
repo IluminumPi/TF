5556e007cac873f4c2e2247284ec4ea3e1a1fef1
IluminumPi <102076151+IluminumPi@users.noreply.github.com>
2025-06-11 11:14:02 -0300

    Add files via upload

---
+++ TRABALHO_FINAL/Equipamento.java
@@ -0,0 +1,35 @@
+
+/**
+ * Escreva uma descrição da classe Equipamento aqui.
+ * 
+ * @author (seu nome) 
+ * @version (um número da versão ou uma data)
+ */
+public class Equipamento
+{
+    private String nome;
+    private String mobilidade;
+    private String qualidade;
+    private int número;
+    
+    public Equipamento(int p, String w, String r, String a){
+        número = p;
+        mobilidade = w;
+        qualidade = r;
+        nome = a;
+       this.qualidade = qualidade; 
+       this.mobilidade = mobilidade;
+       this.número = número;
+       this.nome = nome;
+    }
+    
+    public String getNome(){ return nome;}
+    public int getNúmero(){ return número; }
+    public String getMobilidade(){ return mobilidade; }
+    public String getQualidade(){ return qualidade; }
+    
+    
+    public String toString(){
+        return " Nome: " + nome + "\n Número: " + número + "\n Qualidade: " + qualidade + "\n Mobilidade: " + mobilidade + "\n";
+    }
+}
